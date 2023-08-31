49f184f3391d8d37febb3fde7fc47ff973e0706e
Mert Ergün <mertergun17@hotmail.com>
2023-08-31 12:55:36 +0300

    Added YOLO bubble detector

---
+++ YOLO/yolo_test.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from ultralytics import YOLO
+
+# Load the YOLO model
+model = YOLO('best.pt')
+
+# Path to the input image
+image_path = '../Test/testfile2.png'
+
+# Perform object detection
+results = model(image_path)
+
+for r in results:
+    im_array = r.plot()
+    im = Image.fromarray(im_array)
+    im.save('output.png')
+
+
+def bubbleFinder(image):
+    results = model(image)
+    
+    # Return a list of coordinates of the bubbles in the image
+    bubble_coordinates = []
+
+    for r in results:
+        for box in r.boxes:
+            # Get xyxy coordinates
+            coordts = box.xyxy
+            # Get the coordinates of the bubble
+            bubble_coordinates.append(coordts)
+
+    return bubble_coordinates
+
+
+print(bubbleFinder(image_path))
